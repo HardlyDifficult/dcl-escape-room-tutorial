44800de213ba106c5fb0dfac55c6516828a16311
NickCuso <hardlydiff@gmail.com>
2019-07-14 17:56:06 -0700

    lint

--- 04-keypad-complete/src/game.ts
+++ 04-keypad-complete/src/game.ts
@@ -4,6 +4,7 @@ import { TransformSystem } from "./modules/transfromSystem";
 
 const transformSystem = new TransformSystem();
 engine.addSystem(transformSystem);
+
 const gameCanvas = new UICanvas();
 gameCanvas.visible = false;
 
