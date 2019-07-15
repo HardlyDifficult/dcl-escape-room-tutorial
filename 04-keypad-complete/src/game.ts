644e955db0f4bae7afc8671ba5d4249711fc9e84
NickCuso <hardlydiff@gmail.com>
2019-07-14 17:38:22 -0700

    Rename

--- 04-keypad-complete/src/game.ts
+++ 04-keypad-complete/src/game.ts
@@ -1,6 +1,9 @@
 import { CreateRoom4 } from "./scenes/room4";
 import { CreateMainScene } from "./scenes/mainScene";
+import { TransformSystem } from "./modules/transfromSystem";
 
+const transformSystem = new TransformSystem();
+engine.addSystem(transformSystem);
 const gameCanvas = new UICanvas();
 gameCanvas.visible = false;
 
