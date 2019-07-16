3aa3fb9f33794d353fb88fc70363cbb48415bcf1
Explosive-James <theexplosivejames@gmail.com>
2019-07-16 18:23:08 +0100

    Door Object merged into Room1

--- 01-door-complete/src/scenes/room1.ts
+++ 01-door-complete/src/scenes/room1.ts
@@ -1,17 +1,37 @@
-import { Door } from "../gameObjects/door";
-
 /**
  * Room 1: Open a door
  */
 export function CreateRoom1(): void {
-  let door = new Door({ position: new Vector3(6.58, 0, 7.85) });
 
-  /**
-   * Add an OnClick event handler to `openDoor`.
-   */
-  door.addComponent(
-    new OnClick((): void => {
-      door.openDoor();
-    })
-  );
+  // Variable to Store is the Door is Open
+  let isDoorOpen = false;
+
+  // Creating Door Object
+  let door = new Entity();
+
+  // Adding Door Model
+  door.addComponent(new GLTFShape("models/generic/door.glb"));
+
+  // Setting Position of the Door
+  door.addComponent(new Transform({ position: new Vector3(6.58, 0, 7.85) }));
+
+  // Creating Door Animation
+  let doorAnimation = new Animator();
+  doorAnimation.addClip(new AnimationState("Open", {looping: false}));
+
+  // Adding Animation to the Door
+  door.addComponent(doorAnimation);
+  // Adding Sound Component
+  door.addComponent(new AudioSource(new AudioClip("sounds/door_squeak.mp3")));
+
+  // Adding Open Door Event
+  door.addComponent(new OnClick(event =>{
+    if(!isDoorOpen){
+      isDoorOpen = true;
+      doorAnimation.getClip("Open").play();
+      door.getComponent(AudioSource).playOnce();
+    }
+  }));
+
+  engine.addEntity(door);
 }
