27e8206f3fd3fff7f91199c0c7fbb0cef74d47d0
NickCuso <hardlydiff@gmail.com>
2019-07-16 18:00:43 +0000

    auto-lint

--- 01-door-complete/src/game.ts
+++ 01-door-complete/src/game.ts
@@ -1,8 +1,9 @@
-
 // Creating Main Scene
 let baseScene = new Entity();
 baseScene.addComponent(new GLTFShape("models/scene.glb"));
-baseScene.addComponent(new Transform({ rotation: Quaternion.Euler(0, 180, 0) }));
+baseScene.addComponent(
+  new Transform({ rotation: Quaternion.Euler(0, 180, 0) })
+);
 
 // Adding Main Scene into Engine
 engine.addEntity(baseScene);
@@ -21,7 +22,7 @@ door.addComponent(new Transform({ position: new Vector3(6.58, 0, 7.85) }));
 
 // Creating Door Animation
 let doorAnimation = new Animator();
-doorAnimation.addClip(new AnimationState("Open", {looping: false}));
+doorAnimation.addClip(new AnimationState("Open", { looping: false }));
 
 // Adding Animation to the Door
 door.addComponent(doorAnimation);
@@ -29,13 +30,15 @@ door.addComponent(doorAnimation);
 door.addComponent(new AudioSource(new AudioClip("sounds/door_squeak.mp3")));
 
 // Adding Open Door Event
-door.addComponent(new OnClick(event =>{
-  if(!isDoorOpen){
-    isDoorOpen = true;
-    doorAnimation.getClip("Open").play();
-    door.getComponent(AudioSource).playOnce();
-  }
-}));
+door.addComponent(
+  new OnClick(event => {
+    if (!isDoorOpen) {
+      isDoorOpen = true;
+      doorAnimation.getClip("Open").play();
+      door.getComponent(AudioSource).playOnce();
+    }
+  })
+);
 
 // Adding Door into Engine
 engine.addEntity(door);
