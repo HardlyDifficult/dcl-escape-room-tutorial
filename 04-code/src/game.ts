c21b121bbdb020a12ce698697f80c697fb22a9e7
Explosive-James <theexplosivejames@gmail.com>
2019-07-16 18:30:13 +0100

    Removed room4

--- 04-code/src/game.ts
+++
@@ -1,8 +0,0 @@
-import { CreateRoom4 } from "./scenes/room4";
-import { CreateMainScene } from "./scenes/mainScene";
-
-const gameCanvas = new UICanvas();
-gameCanvas.visible = false;
-
-CreateMainScene();
-CreateRoom4(gameCanvas);
