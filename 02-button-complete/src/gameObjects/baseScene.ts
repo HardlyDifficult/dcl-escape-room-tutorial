19943c7983f8d89c17c397c0c51978c46e982678
James <28958050+Explosive-James@users.noreply.github.com>
2019-07-16 22:08:26 +0100

    Files Combined (#25)
    * Consistent names and patterns
    
    * Merged 02-Complete & 02-Playground files into their respective game.ts files
    
    * auto-lint

--- 02-button-complete/src/gameObjects/baseScene.ts
+++
@@ -1,9 +0,0 @@
-export class BaseScene extends Entity {
-  constructor() {
-    super();
-    engine.addEntity(this);
-
-    this.addComponent(new GLTFShape("models/scene.glb"));
-    this.addComponent(new Transform({ rotation: Quaternion.Euler(0, 180, 0) }));
-  }
-}
--- 02-button-playground/src/game.ts
+++ 02-button-playground/src/game.ts
@@ -3,4 +3,10 @@ import { CreateMainScene } from "./scenes/mainScene";
 /**
  * Load the game scene by scene.
  */
-CreateMainScene();
+let baseScene = new Entity();
+engine.addEntity(baseScene);
+
+baseScene.addComponent(new GLTFShape("models/scene.glb"));
+baseScene.addComponent(
+  new Transform({ rotation: Quaternion.Euler(0, 180, 0) })
+);
