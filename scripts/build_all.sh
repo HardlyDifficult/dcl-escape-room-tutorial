f41401d99f3c17317891e1e5474733ada252136b
Hardly Difficult <hardlydiff@gmail.com>
2019-08-01 16:51:56 -0700

    Update build_all.sh

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -2,7 +2,7 @@ set -e
 
 #!/usr/bin/env bash
 ls | grep -E "^[0-9]{2}" | while read -r folder ; do
-    echo "Starting deploy for $folder"
+    echo "Starting build for $folder"
     cd "$folder"
     npm i
     npm run build
