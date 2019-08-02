4a7318c1e4ad551d959f76864fb1107e57718058
NickCuso <HardlyDiff@gmail.com>
2019-08-02 11:32:11 -0700

    Sort imports by usage and add previous rooms

--- 02-button-refactored/src/game.ts
+++ 02-button-refactored/src/game.ts
@@ -1,5 +1,7 @@
-import { CreateRoom2 } from "./scenes/room2";
 import { BaseScene } from "./gameObjects/baseScene";
+import { CreateRoom1 } from "./scenes/room1";
+import { CreateRoom2 } from "./scenes/room2";
 
 new BaseScene();
+CreateRoom1();
 CreateRoom2();
