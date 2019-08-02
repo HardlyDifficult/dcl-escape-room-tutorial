ac159fb9f84d59b86b0b847ea7ebe78cc0cafe80
NickCuso <HardlyDiff@gmail.com>
2019-08-02 10:53:42 -0700

    02-playground updates

--- 02-button-playground/src/scenes/room2.ts
+++ 02-button-playground/src/scenes/room2.ts
@@ -1,4 +1,6 @@
 // DCL provided components used by this room
 import utils from "../../node_modules/decentraland-ecs-utils/index";
 
-export function CreateRoom2(): void {}
+export function CreateRoom2(): void {
+  // TODO create room 2
+}
