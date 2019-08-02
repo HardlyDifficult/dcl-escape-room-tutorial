81a45c6a7d71c1484684ac5f4d7b2866f7fee482
NickCuso <HardlyDiff@gmail.com>
2019-08-02 13:00:36 -0700

    Comment room 2

--- 02-button-refactored/src/scenes/room2.ts
+++ 02-button-refactored/src/scenes/room2.ts
@@ -1,64 +1,59 @@
-// DCL provided components used by this room
 import utils from "../../node_modules/decentraland-ecs-utils/index";
+// Various game objects used in this room
 import { Door } from "../gameObjects/door";
 import { Button } from "../gameObjects/button";
 import { Timer } from "../gameObjects/timer";
 
-export function CreateRoom2(): void {
-  /**
-   * Door
-   */
+const openDoorTime = 5;
 
+export function CreateRoom2(): void {
+  // Create a door with a look and feel specific to this room
   const door = new Door(
     new GLTFShape("models/room2/Puzzle02_Door.glb"),
-    new Transform({
-      position: new Vector3(24.1, 5.51634, 24.9)
-    }),
+    {position: new Vector3(24.1, 5.51634, 24.9)}, 
     new AudioClip("sounds/door_squeak.mp3")
   );
 
-  /**
-   * Countdown timer
-   */
-
-  // Add a model to display the countdown timer on the wall
+  // Create the timer on the wall
   const countdownClock = new Timer(
-    new Transform({
-      position: new Vector3(25.1272, 9.51119, 25.1116)
-    })
-  );
+    new GLTFShape("models/room1/Countdown_Clock.glb"),
+      {position: new Vector3(25.1272, 9.51119, 25.1116)});
+  // and set the default value
+  countdownClock.updateTimeString(openDoorTime);
 
-  /**
-   * Button
-   */
-
-  // Add the Button the we'll use to open the Door
+  // Create a button
   const button = new Button(
     new GLTFShape("models/room2/Square_Button.glb"),
-    new Transform({
-      position: new Vector3(26.3714, 6.89, 26.8936)
-    })
+    {position: new Vector3(26.3714, 6.89, 26.8936)}
   );
 
-  // When the player clicks the button
   button.addComponent(
     new OnClick((): void => {
       if (!countdownClock.hasComponent(utils.Interval)) {
-        let timeRemaining = countdownClock.openDoorTime;
-
+        // Play the press effect
+        button.press();
+        // Open the door
+        door.openDoor();
+        
+        let timeRemaining = openDoorTime;
         countdownClock.addComponent(
           new utils.Interval(1000, (): void => {
             timeRemaining--;
-            countdownClock.updateTimeString(timeRemaining);
 
-            if (timeRemaining <= 0) {
+            if (timeRemaining > 0) {
+              // Use the updateTime helper
+              countdownClock.updateTimeString(timeRemaining);
+            } else {
               countdownClock.removeComponent(utils.Interval);
+
+              // Close the door
               door.closeDoor();
+
+              // Then reset the text
+              countdownClock.updateTimeString(openDoorTime);
             }
           })
         );
-
-        door.openDoor();
       }
     })
   );
