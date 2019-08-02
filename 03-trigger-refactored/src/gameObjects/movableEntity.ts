441111c07cd08d1a2d319608a6029193d41074cd
James <28958050+Explosive-James@users.noreply.github.com>
2019-08-03 00:34:48 +0100

    Room 03 Refactored (#65)
    * Room 03 Refactored
    
    * Merging 01-02 into 03 refactored

---
+++ 03-trigger-refactored/src/gameObjects/movableEntity.ts
@@ -0,0 +1,54 @@
+import utils from "../../node_modules/decentraland-ecs-utils/index";
+
+/**
+ * An object which moves from one position to another when toggled.
+ */
+export class MovableEntity extends Entity {
+  constructor(
+    model: GLTFShape,
+    transform: TranformConstructorArgs,
+    sound: AudioClip,
+    deltaPosition: Vector3
+  ) {
+    super();
+    engine.addEntity(this);
+
+    this.addComponent(model);
+    this.addComponent(new Transform(transform));
+    this.addComponent(new AudioSource(sound));
+
+    // Save the positions to move between
+    const startPos = transform.position;
+    const endPos = transform.position.add(deltaPosition);
+
+    // Add a Toggle component which defaults to Off
+    this.addComponent(
+      new utils.ToggleComponent(utils.ToggleState.Off, (value): void => {
+        // On change
+
+        if (value == utils.ToggleState.On) {
+          // Move to the endPos when toggled on
+          this.addComponentOrReplace(
+            new utils.MoveTransformComponent(
+              this.getComponent(Transform).position,
+              endPos,
+              0.5
+            )
+          );
+        } else {
+          // Move to the startPos when toggled off
+          this.addComponentOrReplace(
+            new utils.MoveTransformComponent(
+              this.getComponent(Transform).position,
+              startPos,
+              0.5
+            )
+          );
+        }
+
+        // And play the sound effect
+        this.getComponent(AudioSource).playOnce();
+      })
+    );
+  }
+}
---
+++ 03-trigger-refactored/src/scenes/room2.ts
@@ -0,0 +1,60 @@
+import utils from "../../node_modules/decentraland-ecs-utils/index";
+// Various game objects used in this room
+import { Door } from "../gameObjects/door";
+import { Button } from "../gameObjects/button";
+import { Timer } from "../gameObjects/timer";
+
+const openDoorTime = 5;
+
+export function CreateRoom2(): void {
+  // Create a door with a look and feel specific to this room
+  const door = new Door(
+    new GLTFShape("models/room2/Puzzle02_Door.glb"),
+    { position: new Vector3(24.1, 5.51634, 24.9) },
+    new AudioClip("sounds/door_squeak.mp3")
+  );
+
+  // Create the timer on the wall
+  const countdownClock = new Timer(
+    new GLTFShape("models/room1/Countdown_Clock.glb"),
+    { position: new Vector3(25.1272, 9.51119, 25.1116) }
+  );
+  // and set the default value
+  countdownClock.updateTimeString(openDoorTime);
+
+  // Create a button
+  const button = new Button(new GLTFShape("models/room2/Square_Button.glb"), {
+    position: new Vector3(26.3714, 6.89, 26.8936)
+  });
+
+  button.addComponent(
+    new OnClick((): void => {
+      if (!countdownClock.hasComponent(utils.Interval)) {
+        // Play the press effect
+        button.press();
+        // Open the door
+        door.openDoor();
+
+        let timeRemaining = openDoorTime;
+        countdownClock.addComponent(
+          new utils.Interval(1000, (): void => {
+            timeRemaining--;
+
+            if (timeRemaining > 0) {
+              // Use the updateTime helper
+              countdownClock.updateTimeString(timeRemaining);
+            } else {
+              countdownClock.removeComponent(utils.Interval);
+
+              // Close the door
+              door.closeDoor();
+
+              // Then reset the text
+              countdownClock.updateTimeString(openDoorTime);
+            }
+          })
+        );
+      }
+    })
+  );
+}
