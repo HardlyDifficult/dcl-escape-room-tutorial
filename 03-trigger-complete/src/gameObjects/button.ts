d5aa970e43b6104ae117fcaa53c6edc443c9a582
NickCuso <HardlyDiff@gmail.com>
2019-08-02 13:36:47 -0700

    03 Button and transform updates

--- 03-trigger-complete/src/gameObjects/button.ts
+++ 03-trigger-complete/src/gameObjects/button.ts
@@ -1,9 +1,9 @@
 export class Button extends Entity {
-  constructor(transform: TranformConstructorArgs) {
+  constructor(model: GLTFShape, transform: TranformConstructorArgs) {
     super();
     engine.addEntity(this);
 
-    this.addComponent(new GLTFShape("models/generic/Round_Button.glb"));
+    this.addComponent(model);
     this.addComponent(new Transform(transform));
 
     this.addComponent(new AudioSource(new AudioClip("sounds/button.mp3")));
