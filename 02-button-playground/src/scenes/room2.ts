728ae830b19fb3bbcd9d09907ce7caba8576ba95
NickCuso <hardlydiff@gmail.com>
2019-08-02 12:59:39 +0000

    auto-lint

--- 02-button-playground/src/scenes/room2.ts
+++ 02-button-playground/src/scenes/room2.ts
@@ -1,6 +1,4 @@
 // DCL provided components used by this room
 import utils from "../../node_modules/decentraland-ecs-utils/index";
 
-export function CreateRoom2(): void {
-
-}
+export function CreateRoom2(): void {}
