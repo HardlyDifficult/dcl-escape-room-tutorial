6b2c5bdbf4f622c7c2d6a9edf820967e89f2c16c
NickCuso <HardlyDiff@gmail.com>
2019-08-02 11:41:57 -0700

    baseScene: comment the pattern

--- 02-button-refactored/src/gameObjects/baseScene.ts
+++ 02-button-refactored/src/gameObjects/baseScene.ts
@@ -1,7 +1,20 @@
+/**
+ * The base scene model
+ */
+
+/**
+ * Export the class to be used by another file
+ * Extends a basic `Entity` so additional components may be added go GameObjects when needed
+ */ 
 export class BaseScene extends Entity {
+  // The constructor is required for the class, called when `new BaseScene()` is used
   constructor() {
+    // First call the Entity constructor
     super();
+    // Then add this object to the engine for rendering
     engine.addEntity(this);
+
+    // Add components which always apply to this GameObject
     this.addComponent(new GLTFShape("models/scene.glb"));
   }
 }
