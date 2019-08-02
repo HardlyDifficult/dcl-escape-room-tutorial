e858060deee14bc57b959b9d831ce6cdfa6ad957
NickCuso <HardlyDiff@gmail.com>
2019-08-02 11:31:15 -0700

    Add the full game to date in -refactored folders

---
+++ 02-button-refactored/src/scenes/room1.ts
@@ -0,0 +1,18 @@
+// Move the door logic to a separate gameObject file
+import { Door } from "../gameObjects/door";
+
+export function CreateRoom1(): void {
+  // Create a door with a look and feel specific to this room
+  const door = new Door(
+    new GLTFShape("models/room1/Puzzle01_Door.glb"),
+    { position: new Vector3(21.18, 10.8, 24.5) },
+    new AudioClip("sounds/door_squeak.mp3")
+  );
+  
+  door.addComponent(
+    new OnClick((): void => {
+      // In this room, the door opens when clicked
+      door.openDoor();
+    })
+  );
+}
