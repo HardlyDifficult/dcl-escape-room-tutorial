b575d0b971117b43f43d05efbb839dfb7efe72c1
NickCuso <HardlyDiff@gmail.com>
2019-08-06 10:49:29 -0700

    Simplify 04

--- 04-bookshelf-complete/src/gameObjects/movableEntity.ts
+++ 04-bookshelf-complete/src/gameObjects/movableEntity.ts
@@ -1,14 +1,13 @@
 import utils from "../../node_modules/decentraland-ecs-utils/index";
 
-/**
- * An object which moves from one position to another when toggled.
- */
 export class MovableEntity extends Entity {
   constructor(
     model: GLTFShape,
     transform: TranformConstructorArgs,
     sound: AudioClip,
-    deltaPosition: Vector3
+    deltaPosition: Vector3,
+    // Add a variable move time with a default value
+    moveTime: number = 0.5 
   ) {
     super();
     engine.addEntity(this);
@@ -17,36 +16,29 @@ export class MovableEntity extends Entity {
     this.addComponent(new Transform(transform));
     this.addComponent(new AudioSource(sound));
 
-    // Save the positions to move between
     const startPos = transform.position;
     const endPos = transform.position.add(deltaPosition);
 
-    // Add a Toggle component which defaults to Off
     this.addComponent(
       new utils.ToggleComponent(utils.ToggleState.Off, (value): void => {
-        // On change
-
         if (value == utils.ToggleState.On) {
-          // Move to the endPos when toggled on
           this.addComponentOrReplace(
             new utils.MoveTransformComponent(
               this.getComponent(Transform).position,
               endPos,
-              0.5
+              moveTime
             )
           );
         } else {
-          // Move to the startPos when toggled off
           this.addComponentOrReplace(
             new utils.MoveTransformComponent(
               this.getComponent(Transform).position,
               startPos,
-              0.5
+              moveTime
             )
           );
         }
 
-        // And play the sound effect
         this.getComponent(AudioSource).playOnce();
       })
     );
