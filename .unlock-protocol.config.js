6fced7628092e6cd0c7f137ca25db8e6a6ac6b99
Hardly Difficult <hardlydiff@gmail.com>
2019-08-28 21:34:21 -0700

    Create .unlock-protocol.config.js

---
+++ .unlock-protocol.config.js
@@ -0,0 +1,14 @@
+{
+  "persistentCheckout": true,
+  "icon": "https://decentraland.org/blog/images/logo.svg",
+  "callToAction": {
+    "default": "Show you support for tutorials like this",
+    "pending": "Thank you so much!",
+    "confirmed": "Thank you so much!"
+  },
+  "locks": {
+    "0x854919067FE146FF16BB1A1d319b55Af92E71EFd": {
+      "name": "A bit of Ether"
+    }
+  }
+}
