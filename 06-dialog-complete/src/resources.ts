a962443ba23092024c399010c016b8fe9c02706a
NickCuso <hardlydiff@gmail.com>
2019-09-11 21:36:54 -0700

    Lint

--- 06-dialog-complete/src/resources.ts
+++ 06-dialog-complete/src/resources.ts
@@ -17,7 +17,6 @@ export default {
     door6: new GLTFShape("models/room6/Puzzle06_Door.glb"),
     muna: new GLTFShape("models/room6/Puzzle06_Muna.glb"),
     numpad2: new GLTFShape("models/room6/Numpad2.glb")
-
   },
   textures: {
     clearButton: new Texture("images/codepad/pwdpanel_clear.png"),
@@ -26,12 +25,25 @@ export default {
     inputBox: new Texture("images/codepad/pwdpanel_input.png"),
     numberButton: new Texture("images/codepad/pwdpanel_buttons.png"),
     panelBackground: new Texture("images/codepad/pwdpanel_bg.png"),
-    
-  playerPortraitDefault: new Texture("images/dialogs/player_default.png", {hasAlpha: true}),
-  playerPortraitSurprised: new Texture("images/dialogs/player_surprised.png", {hasAlpha: true}),
-  playerPortraitThinking: new Texture("images/dialogs/player_thinking.png", {hasAlpha: true}),
-  npcPortraitDefault: new Texture("images/dialogs/dog_default.png", {hasAlpha: true}),
-  npcPortraitSurprised: new Texture("images/dialogs/dog_surprised.png", {hasAlpha: true}),
-  npcPortraitThinking: new Texture("images/dialogs/dog_thinking.png", {hasAlpha: true})
+
+    playerPortraitDefault: new Texture("images/dialogs/player_default.png", {
+      hasAlpha: true
+    }),
+    playerPortraitSurprised: new Texture(
+      "images/dialogs/player_surprised.png",
+      { hasAlpha: true }
+    ),
+    playerPortraitThinking: new Texture("images/dialogs/player_thinking.png", {
+      hasAlpha: true
+    }),
+    npcPortraitDefault: new Texture("images/dialogs/dog_default.png", {
+      hasAlpha: true
+    }),
+    npcPortraitSurprised: new Texture("images/dialogs/dog_surprised.png", {
+      hasAlpha: true
+    }),
+    npcPortraitThinking: new Texture("images/dialogs/dog_thinking.png", {
+      hasAlpha: true
+    })
   }
 };
