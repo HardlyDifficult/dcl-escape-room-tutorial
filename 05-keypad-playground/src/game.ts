a67a90a90b752cb714e1b56b65f82d660cf18bd2
Hardly Difficult <hardlydiff@gmail.com>
2019-09-12 11:27:35 -0700

    Update 05 playground (#123)
    * Update 05 playground
    
    * Remove hintImage variable

--- 05-keypad-playground/src/game.ts
+++ 05-keypad-playground/src/game.ts
@@ -1,8 +1,5 @@
 import { CreateRoom5 } from "./scenes/room5";
 import { BaseScene } from "./gameObjects/index";
 
-// Add a canvas for UI elements
-const gameCanvas = new UICanvas();
-
 new BaseScene();
-CreateRoom5(gameCanvas);
+CreateRoom5();
