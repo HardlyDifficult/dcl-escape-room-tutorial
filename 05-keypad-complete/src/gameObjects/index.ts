f86cf812f421c18ed5a58f5d943632fc7f5c3ab6
Hardly Difficult <hardlydiff@gmail.com>
2019-09-13 10:29:28 -0700

    Fix casing (#129)

--- 05-keypad-complete/src/gameObjects/index.ts
+++ 05-keypad-complete/src/gameObjects/index.ts
@@ -1,7 +1,7 @@
 import { BaseScene } from "./baseScene";
 import { Door } from "./door";
 import { Model } from "./model";
-import { NumPadLock } from "./NumPadLock";
+import { NumPadLock } from "./numPadLock";
 import { RotatableEntity } from "./rotatableEntity";
 
 export { BaseScene, Door, Model, NumPadLock, RotatableEntity };
