16e664b619f3131cd07cb355e0546d64c8d15a23
NickCuso <hardlydiff@gmail.com>
2019-09-13 15:33:28 -0700

    Cleanup template

--- 06-dialog-playground/src/scenes/room6.ts
+++ 06-dialog-playground/src/scenes/room6.ts
@@ -1,11 +1,6 @@
-import { SimpleDialog } from "../modules/simpleDialog";
 import resources from "../resources";
 import { MovableEntity, NumPadLock, Door } from "../gameObjects/index";
 import { Keypad, MunaDialog } from "../ui/index";
-
-function selectRandom(options: string[]): string {
-  return options[Math.floor(Math.random() * (options.length - 1))];
-}
 
 export function CreateRoom6(gameCanvas: UICanvas): void {
   // ToDo Create Room6
