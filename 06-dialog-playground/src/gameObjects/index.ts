166fb6fcd8fcae87a40498358e3be60c113e206c
NickCuso <hardlydiff@gmail.com>
2019-09-13 10:25:00 -0700

    Fix casing

--- 06-dialog-playground/src/gameObjects/index.ts
+++ 06-dialog-playground/src/gameObjects/index.ts
@@ -2,7 +2,7 @@ import { BaseScene } from "./baseScene";
 import { Door } from "./door";
 import { Model } from "./model";
 import { MovableEntity } from "./movableEntity";
-import { NumPadLock } from "./NumPadLock";
+import { NumPadLock } from "./numPadLock";
 import { RotatableEntity } from "./rotatableEntity";
 
 export { BaseScene, Door, Model, MovableEntity, NumPadLock, RotatableEntity };
