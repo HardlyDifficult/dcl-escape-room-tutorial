067a0c5098425d75767d2b053cfd8aa2d754d223
NickCuso <hardlydiff@gmail.com>
2019-09-16 17:00:00 +0000

    auto-lint

--- 09-kitchen-complete/src/scenes/room9.ts
+++ 09-kitchen-complete/src/scenes/room9.ts
@@ -15,11 +15,11 @@ import { MouseFallingState } from "../stateMachine/mouseFallingState";
 import { MouseComponent } from "../components/mouseComponent";
 
 //set trigger layers
-const MouseLayer = 8;   //     1000 in binary
-const PikesLayer = 16;  //    10000
-const BoxLayer = 32;    //   100000
-const FanLayer = 64;    //  1000000
-const CageLayer = 128;  // 10000000
+const MouseLayer = 8; //     1000 in binary
+const PikesLayer = 16; //    10000
+const BoxLayer = 32; //   100000
+const FanLayer = 64; //  1000000
+const CageLayer = 128; // 10000000
 // e.g.                    10100000 means cage or box
 
 export function CreateRoom9(): void {
